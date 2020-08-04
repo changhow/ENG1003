087049ba92c38b8f11bce75f8b8d4ecab33f54de
chang <chang.how.tan@monash.edu>
2020-08-04 11:15:55 +1000

    resolve conflict

--- hello_word.js
+++ hello_word.js
@@ -2,8 +2,4 @@ console.log("helloworld");
 console.log("conflict 2")
 
 
-<<<<<<< HEAD
 console.log("conflict 3");
-=======
-console.log("conflict test")
->>>>>>> eb90ae432cf168c5f4006395b6914b9a6b5be5c7
